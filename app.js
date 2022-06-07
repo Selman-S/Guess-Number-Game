f4743d57bdb33d0a43516c88c4ef24751c175c79
selman <gunduzgece546@gmail.com>
2022-06-07 22:51:33 +0300

    start page

--- app.js
+++ app.js
@@ -1,2 +1,87 @@
 
+const difficulty= document.querySelectorAll('.difficulty');
+let intro =document.getElementById("intro");
 
+
+
+difficulty.forEach(function (node,i) {
+
+  node.addEventListener('click',(a)=>{
+    if(i==0){
+      
+      a.target.parentNode.nextElementSibling.classList.add('disabled');
+
+      a.target.parentNode.nextElementSibling.nextElementSibling.classList.add('disabled');
+
+      a.target.parentNode.nextElementSibling.classList.remove('d-hover');
+
+      a.target.parentNode.nextElementSibling.nextElementSibling.classList.remove('d-hover');
+
+      a.target.parentNode.classList.remove('d-hover');
+
+      document.getElementById("description").innerHTML = "Let's start";
+
+      document.querySelector('.start').classList.add('s-hover');
+
+      document.querySelector('.start').classList.remove('disabled');
+
+      sessionStorage.setItem('diffic',10);
+
+      document.querySelector('#intro').innerText = `Guess The Number (1 -  ${sessionStorage.getItem('diffic')})`
+      document.querySelector('.start').disabled = false;
+      a.target.parentNode.disabled =true;
+      
+    }if(i==1){
+      a.target.parentNode.nextElementSibling.classList.add('disabled');
+      a.target.parentNode.previousElementSibling.classList.add('disabled');
+      a.target.parentNode.nextElementSibling.classList.remove('d-hover');
+      a.target.parentNode.previousElementSibling.classList.remove('d-hover');
+
+      a.target.parentNode.classList.remove('d-hover');
+
+      document.getElementById("description").innerHTML = "Let's start";
+
+      document.querySelector('.start').classList.add('s-hover');
+
+      document.querySelector('.start').classList.remove('disabled');
+
+      sessionStorage.setItem('diffic',100);
+
+      document.querySelector('#intro').innerText = `Guess The Number (1 -  ${sessionStorage.getItem('diffic')})`
+      document.querySelector('.start').disabled = false;
+      a.target.parentNode.disabled =true;
+      
+      
+      
+      
+      
+     
+    }if(i==2){
+      a.target.parentNode.previousElementSibling.classList.add('disabled');
+      a.target.parentNode.previousElementSibling.previousElementSibling.classList.add('disabled');
+      a.target.parentNode.previousElementSibling.classList.remove('d-hover');
+      a.target.parentNode.previousElementSibling.previousElementSibling.classList.remove('d-hover');
+
+      a.target.parentNode.classList.remove('d-hover');
+
+      document.getElementById("description").innerHTML = "Let's start";
+
+      document.querySelector('.start').classList.add('s-hover');
+
+      document.querySelector('.start').classList.remove('disabled');
+
+      sessionStorage.setItem('diffic',1000);
+
+      document.querySelector('#intro').innerText = `Guess The Number (1 -  ${sessionStorage.getItem('diffic')})`
+      document.querySelector('.start').disabled = false;
+      a.target.parentNode.disabled =true;
+    }
+    
+  })
+  
+  
+  
+})
+
+
+console.log(sessionStorage.getItem('diffic'));
