fd97e8da6540d4c08f4d0f4895d94915b769b2a7
selman <gunduzgece546@gmail.com>
2022-06-08 02:01:06 +0300

    restart

--- app.js
+++ app.js
@@ -93,13 +93,48 @@ const start = document.getElementById('start');
 
 
 start.addEventListener('click', () =>{
-  document.getElementById("intro-page").style.display="none";
-  document.querySelector(".guess").max = "100";
-  document.querySelector('#intro').innerText = `Guess The Number `
-  document.querySelector("#between").innerText = `Please enter a number between 1 and ${sessionStorage.getItem('diffic')} `;
+  if(document.getElementById('start').disabled==false){
+
+    document.getElementById("intro-page").style.display="none";
+
+    document.querySelector(".guess").max = "100";
+
+    document.querySelector('#intro').innerText = `Guess The Number `;
+
+    document.querySelector("#between").innerText = `Please enter a number between 1 and ${sessionStorage.getItem('diffic')} `;
+
+    const randomNum =Math.floor(Math.random()*sessionStorage.getItem('diffic')+1);
+
+    console.log(randomNum);
+
+    document.querySelector('.guess-page').style.display="flex";
+    document.querySelector('.guess.form-control').autofocus=true;
+    document.getElementById('check').addEventListener('click', () =>{
+      if(document.querySelector('.guess').value>100 || document.querySelector('.guess').value>0){
+
+      }
+      if(document.querySelector('.guess').value==randomNum){
+        document.getElementById("result").innerText = "Congratulations.. You win";
+        document.querySelector('.input-page').style.display="none";
+        document.querySelector('.conres').style.display="flex";
+        
+      }
+      
+
+  
+    })
+  }
 
 
   
+})
+document.getElementById('restart').addEventListener('click', ()=>{
+  document.querySelector('.guess-page').style.display="none";
+  document.getElementById("intro-page").style.display="block";
+  
+
+
+ 
 })
 
 
